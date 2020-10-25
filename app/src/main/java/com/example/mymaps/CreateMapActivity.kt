5eb807af3a7d2e3bb7a0f2aec9bddbee8a3c0f0c
Paul Boehringer <paulboehringer0989@gmail.com>
2020-10-25 15:57:00 -0400

    First commit

---
+++ app/src/main/java/com/example/mymaps/CreateMapActivity.kt
@@ -0,0 +1,120 @@
+package com.example.mymaps
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.accessibility.AccessibilityEventCompat.setAction
+import com.example.mymaps.model.Place
+import com.example.mymaps.model.UserMap
+//import androidx.appcompat.app.AlertDialog
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
+
+private const val TAG = "CreateMapActivity"
+class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private var markers: MutableList<Marker> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_map)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        supportActionBar?.title = intent.getStringExtra(EXTRA_MAP_TITLE)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        mapFragment.view?.let {
+            Snackbar.make(it,"Long press to add a marker!", Snackbar.LENGTH_INDEFINITE)
+                .setAction("ok", {})
+                .setActionTextColor(ContextCompat.getColor(this, android.R.color.white))
+                .show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_create_map, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // check if item is in save menu
+        if (item.itemId == R.id.miSave) {
+            Log.i(TAG, "Tapped on save!")
+            if (markers.isEmpty()) {
+                Toast.makeText(this, "there must be atleast one marker", Toast.LENGTH_LONG).show()
+            }
+            val places = markers.map{ marker -> Place(marker.title, marker.snippet, marker.position.latitude, marker.position.longitude)  }
+            val userMap = UserMap(intent.getStringExtra(EXTRA_MAP_TITLE).toString(), places)
+            val data = Intent()
+            data.putExtra(EXTRA_USER_MAP, userMap)
+            setResult(Activity.RESULT_OK, data)
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        mMap.setOnInfoWindowClickListener { markerToDelete ->
+            Log.i(TAG, "onWindowClickListener - delete this marker ")
+            markers.remove(markerToDelete)
+            markerToDelete.remove()
+        }
+        mMap.setOnMapLongClickListener {latLng ->
+            Log.i(TAG, "onMapLongClickListener")
+            showAlertDialog(latLng)
+
+        }
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10f))
+    }
+
+    private fun showAlertDialog(latLng: LatLng) {
+        val placeFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_place, null )
+        val dialog =
+            AlertDialog.Builder(this)
+            .setTitle("Create a marker")
+            .setView(placeFormView)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK", null)
+            .show()
+
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            val title = placeFormView.findViewById<EditText>(R.id.etTitle).text.toString()
+            val description = placeFormView.findViewById<EditText>(R.id.etDescription).text.toString()
+            if (title.trim().isEmpty() || description.trim().isEmpty()) {
+                Toast.makeText(this, "Place must have a title", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            val marker = mMap.addMarker(MarkerOptions().position(latLng).title(title).snippet(description))
+            markers.add(marker)
+            dialog.dismiss()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mymaps/DisplayMapActivity.kt
@@ -0,0 +1,52 @@
+package com.example.mymaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.mymaps.model.UserMap
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+
+private const val TAG = "DisplayMapActivity"
+class DisplayMapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var userMap: UserMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_map)
+
+        userMap = intent.getSerializableExtra(EXTRA_USER_MAP) as UserMap
+
+        supportActionBar?.title = userMap.title
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        Log.i(TAG, "user map to render: ${userMap.title}")
+
+        val boundsBuilder = LatLngBounds.Builder()
+        for (place in userMap.places) {
+            val latLng = LatLng(place.latitude, place.longitude)
+            boundsBuilder.include(latLng)
+            mMap.addMarker(MarkerOptions().position(latLng).title(place.title).snippet(place.description))
+        }
+
+        // Add a marker in Sydney and move the camera
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(), 1000, 1000, 0))
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    }
+}
